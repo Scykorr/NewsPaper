5e3acaf072b92d5c15c9456776763ec2477efbd8
Anton Fedosov <fedosov-toxa@mail.ru>
2023-11-29 16:41:44 +0300

    add first models

--- news/models.py
+++ news/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from datetime import datetime, timezone
 
-# Create your models here.
+
+class Author(models.Model):
+    user_rank = models.IntegerField(default=0)
+
+
+class Category(models.Model):
+    name = models.CharField(unique=True)
+
+
+class Post(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(default='***')
+    text = models.TextField()
+    post_rank = models.IntegerField(default=0)
+
+
+class PostCategory(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    comment_rank = models.IntegerField(default=0)
+
