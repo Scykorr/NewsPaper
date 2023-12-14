de3b5b34f1a9eb0653eef692fada80352e9cfab5
Anton Fedosov <fedosov-toxa@mail.ru>
2023-12-14 16:13:35 +0300

    add pagination

--- news/views.py
+++ news/views.py
@@ -8,6 +8,7 @@ class NewsList(ListView):
     queryset = Post.objects.all().order_by('-create_date')
     template_name = 'news.html'
     context_object_name = 'news'
+    paginate_by = 10
 
 
 class OneNews(DetailView):
