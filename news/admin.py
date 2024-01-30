f97d5219a39659a4389a922942893e7564e7c332
Anton Fedosov <fedosov-toxa@mail.ru>
2024-01-30 10:20:39 +0300

    add pretty sorting in admin.py

--- news/admin.py
+++ news/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from .models import Post, PostCategory, Author, Category, Comment
 
-admin.site.register(Post)
+
+def nullfy_post_rank(modeladmin, request, queryset):
+    queryset.update(post_rank=0)
+
+
+nullfy_post_rank.short_description = 'Обнулить ранк'
+
+
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'post_rank', 'author')
+
+    list_filter = ('title', 'post_rank', 'author')
+
+    search_fields = ('title', 'category__name')
+    actions = [nullfy_post_rank]
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)
 admin.site.register(Author)
 admin.site.register(Category)
