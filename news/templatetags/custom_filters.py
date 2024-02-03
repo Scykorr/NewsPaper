9624adeee90a585231ed989bc7f90ef7fd99a0d6
Tony <fedosov-toxa@mail.ru>
2024-02-03 17:03:50 +0300

    add custom filter

--- news/templatetags/custom_filters.py
+++ news/templatetags/custom_filters.py
@@ -6,7 +6,19 @@ register = template.Library()
 stop_list = [('дурак', 'д****'), ('Дураки', 'Д*****'), ('дура', 'д***'), ('дуры', 'д***'), ('сука', 'с***'),
              ('суки', 'с***'), ('Кобели', 'К*****'), ('тварей', 'т*****')]
 
+forbidden_words = ['дурак', 'Дураки', 'дура', 'дуры', 'сука', 'суки', 'Кобели', 'тварей']
 
 @register.filter(name='censor')
 def currency(value: str):
     return reduce(lambda s, r: s.replace(r[0], r[1]), stop_list, value)
+
+@register.filter
+def hide_forbidden(value):
+    words = value.split()
+    result = []
+    for word in words:
+        if word in forbidden_words:
+            result.append(word[0] + "*"*(len(word)-2) + word[-1])
+        else:
+            result.append(word)
+    return " ".join(result)
